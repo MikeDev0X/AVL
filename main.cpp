58052a2c8724cefcda67cdd32d297d6ce4a88860
Miguel Jiménez <60368313+MikeDev0X@users.noreply.github.com>
2021-11-02 04:39:59 +0000

    Ya funciona xd

--- main.cpp
+++ main.cpp
@@ -6,10 +6,15 @@ using namespace std;
 
 int main() {
   Arbol a1;
-  //a1.setRaiz(a1.insertaAVL(a1.getRaiz(),false,5));
-  a1.insertaAVL(a1.getRaiz(),false,5);
-  //a1.insertaAVL(a1.getRaiz(),false,7);
-  //a1.insertaAVL(a1.getRaiz(),false,4);
+  //a1.setRaiz(new Nodo(5));
+  a1.setRaiz(a1.insertaAVL(a1.getRaiz(),false,14));
+  a1.setRaiz(a1.insertaAVL(a1.getRaiz(),false,5));
+  a1.setRaiz(a1.insertaAVL(a1.getRaiz(),false,34));
+  a1.setRaiz(a1.insertaAVL(a1.getRaiz(),false,3));
+  a1.setRaiz(a1.insertaAVL(a1.getRaiz(),false,7));
+  a1.setRaiz(a1.insertaAVL(a1.getRaiz(),false,9));
   a1.preorder(a1.getRaiz());
+  //cout<<a1.getRaiz()->getDato();
+  //a1.inorder(a1.getRaiz());
 
 }
